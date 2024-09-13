9e3f63ab9ebc0db50e41087a3f3fa30548af87b9
Colina Qiao <xqiao7@jh.edu>
2024-09-13 06:30:02 -0400

    updated day3 afternoon exercise

--- day3-afternoon/exercise.py
+++ day3-afternoon/exercise.py
@@ -35,6 +35,8 @@ for line in fs:
     fields = line.strip("\n").split("\t")
 #   save field 0 into gene IDs
     gene_IDs.append(fields[0])
+#   save field 1 into gene names
+    gene_names.append(fields[1])
 #   save 2+ into expression values
     expression.append(fields[2:])
 fs.close()
@@ -53,15 +55,23 @@ tissue_expression_mean = numpy.mean(expression[:10,:], axis = 1)
 expression_mean = numpy.mean(expression[:,:])
 expression_median = numpy.median(expression[:,:])
 
-#print(expression_mean)
-#print(expression_median)
+print(expression_mean)
+print(expression_median)
+#The mean is much higher than the median, which suggests the data is right-skewed. 
+#This can also mean that a few genes might have a very high expression value while the the
+#majority of the rest might have a low expression value. One needs to consider what 
+#statistical information needs to be extracted from the dataset.
 
 #Q6
 expression_log_1 = numpy.log2(expression + 1)
 expression_log_mean = numpy.mean(expression_log_1)
 expression_log_median = numpy.median(expression_log_1)
-#print(expression_log_mean)
-#print(expression_log_median)
+print(expression_log_mean)
+print(expression_log_median)
+#After log2 transformation, the mean and median are much closer. This is because
+#using log could help reduce the skewness of data.
+#In this case, the log2 transformation helps normalize the data, and thus helps reduce the 
+#influence of a few highly expressed genes.
 
 #7
 expression_log_copy = numpy.copy(expression_log_1)
@@ -78,9 +88,8 @@ expression_1st_2nd_diff = expression_log_copy_sorted[:,-1] - expression_log_copy
 
 #8
 num_greater_10 = numpy.sum(expression_1st_2nd_diff >= 10)
-#print(num_greater_10)
-
-
-
+print(num_greater_10)
+#There are 33 genes whose difference between the highest and second highest tissue 
+#expression is greater than 10.
 
 
